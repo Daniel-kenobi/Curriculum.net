3104aaac3d67470737c71ac1f5e8633803534282
Daniel Felipe <daniel.companyservicostecnicos@gmail.com>
2021-02-14 22:30:34 -0300

    updates

--- Curriculum.net/Startup.cs
+++ Curriculum.net/Startup.cs
@@ -39,7 +39,7 @@ namespace Curriculum.net
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment()) 
+            if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
 
             app.UseRouting();
